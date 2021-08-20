305823e40e1c9b0f76a349b49dbbe102d6c4e212
AnthonyMerive <anthonycolmenaresrivas@gmail.com>
2021-08-20 17:04:40 -0500

    finish loggin

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,10 @@
 let login = document.getElementById('login');
+let registro = document.getElementById('registro');
 
+registro.addEventListener('click', e => {
+    e.preventDefault();
+    window.location.href = "./register.html"
+})
 login.addEventListener('submit', e => {
     e.preventDefault();
     validarUsuario();
@@ -9,31 +14,36 @@ const validarUsuario = () => {
     let usuario = document.getElementById('user').value;
     let password = document.getElementById('password').value;
 
-    let userData = JSON.parse(localStorage.getItem('usuarios'));
-
-    if(userData === null){
-        alert('Usuario no reconocido')
-        window.location.href = "./register.html"
-    }
-
-    let filtro = userData.filter(data => data.user === usuario)
-
-    if (filtro.length == 0) {
-
-        alert('Usuario no reconocido')
-        window.location.href = "./register.html"
+    if (usuario === '' || password === '') {
+        alert('¡Rellene todos los campos!')
     } else {
 
-        filtro.map(data => {
-            const { pass } = data;
-            if (password === pass) {
-                alert('Usuario Reconocido')
-                window.location.href = "./index.html"
-            } else {
-                alert('Usuario no reconocido')
-                window.location.href = "./register.html"
-            }
-        })
+        let userData = JSON.parse(localStorage.getItem('usuarios'));
+
+        if (userData === null) {
+            alert('¡Usuario no encontrado!\n\nRegistrese por favor')
+            window.location.href = "./register.html"
+        }
+
+        let filtro = userData.filter(data => data.user === usuario)
+
+        if (filtro.length == 0) {
+
+            alert('¡Usuario no encontrado!\n\nRegistrese por favor')
+            window.location.href = "./register.html"
+        } else {
+
+            filtro.map(data => {
+                const { pass } = data;
+                if (password === pass) {
+                    alert('Usuario Reconocido')
+                    window.location.href = "./index.html"
+                } else {
+                    alert('¡Usuario no encontrado!\n\nRegistrese por favor')
+                    window.location.href = "./register.html"
+                }
+            })
+        }
     }
 
     //Metodo for que repite las alerts (no optimizado) recorre todos los arrays y los compara
--- scripts/register.js
+++ scripts/register.js
@@ -17,40 +17,42 @@ let dataRenew = [];
 
 
 const registrarUsuario = () => {
-   let dataLocalStorage = JSON.parse(localStorage.getItem('usuarios'));
+    let dataLocalStorage = JSON.parse(localStorage.getItem('usuarios'));
+    let user = document.getElementById('user').value;
+    let pass = document.getElementById('password').value;
 
-    if(dataLocalStorage == null){
+    if (user === '' || pass === '') {
+        alert('¡Rellene todos los campos!')
 
-        let user = document.getElementById('user').value;
-        let pass = document.getElementById('password').value;
-    
+    }else if (dataLocalStorage == null) {  
+
         let usuarios = {
             user,
             pass
         }
-    
+
         dataUser.unshift(usuarios);
         localStorage.setItem('usuarios', JSON.stringify(dataUser));
-    
-        alert('usuario creado correctamente')
-        window.location.href = "./login.html" 
 
-    }else{
+        alert('usuario creado correctamente')
+        window.location.href = "./login.html"
+
+    } else {
 
         let user = document.getElementById('user').value;
         let pass = document.getElementById('password').value;
-    
+
         let usuarios = {
             user,
             pass
         }
         dataRenew.unshift(usuarios);
         let newData = dataRenew.concat(dataLocalStorage);
-              
+
         localStorage.setItem('usuarios', JSON.stringify(newData));
-    
+
         alert('usuario creado correctamente')
-        window.location.href = "./login.html" 
+        window.location.href = "./login.html"
     }
-    
+
 }
